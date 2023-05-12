64c7bd4eaf2a0a1968f84103f784d6d56052c72c
nikunjitaliya2 <nikunj.italiya@ghanshyamdigital.com>
2023-05-12 20:01:42 +0530

    invoice crm assignment

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<script setup>
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <div class='container-fluid border-bottom py-2'>
+    <div class='d-flex justify-content-between align-items-center pt-2'>
+      <div>
+        <h5 class='' style='color: #0F2744;'>Create Invoice</h5>
+        <div class='d-flex'>
+          <h6>cash</h6>
+          <div class='px-2'>
+            <label class='switch'>
+              <input type='checkbox'>
+              <span class='slider round'></span>
+            </label>
+          </div>
+          <h6>Credit</h6>
+        </div>
+      </div>
+      <div class='d-flex align-items-baseline'>
+        <h5 class='common-text'>Balance Due</h5>
+        <h3 class='px-2 text-style-common' style='color: #020C60'>0.00</h3>
+        <p class='pe-2 pe-4'>PRs</p>
+        <i class="fa-solid fa-gear fs-3"></i>
+      </div>
+    </div>
+  </div>
+  <Home/>
+</template>
+
+<style scoped>
+body {
+  background: #E0E0E0;
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 20px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 12px;
+  width: 12px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,253 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+// toggle button
+const value = ref('')
+const discount = ref(0)
+const GstPlus = ref(0)
+const TotalAmount = ref(0)
+
+const ItemList = reactive({
+  itemName: '',
+  Qlt: 0,
+  Uom: '',
+  Rate: 0,
+  Gst: 0,
+  Discount: 0,
+  Amount: 0
+})
+
+const InvoiceDetails = reactive({
+  InvoiceNumber: 0,
+  referenceNumber: 0,
+  // Date: "dd-mm-yyyy",
+  AccountName: '',
+  SalesLedger: ''
+})
+
+const discountTotal = async (Quelity, Rate, DiscountTotal, gst) => {
+  // total discount and Item rate
+  discount.value = (ItemList.Rate * Quelity) - ((DiscountTotal / 100) * (Quelity * Rate))
+  GstPlus.value = (gst / 100) * (discount.value)
+
+  TotalAmount.value = discount.value + GstPlus.value
+  // console.log('total discount ----> ', discount.value)
+  // console.log('total gst ----> ', GstPlus.value)
+  console.log('tTotalAmount.value ----> ', TotalAmount.value)
+}
+
+// without function
+
+// discount.value = (ItemList.Rate * ItemList.Qlt) - ((ItemList.Discount / 100) * ( ItemList.Qlt * ItemList.Rate))
+// GstPlus.value = (ItemList.Gst / 100) * (discount.value)
+// console.log('total discount ----> ', discount.value)
+// console.log('total gst ----> ', GstPlus.value)
+
+const SaveProductDetails = async () => {
+  await discountTotal(ItemList.Qlt, ItemList.Rate, ItemList.Discount, ItemList.Gst)
+}
+
+const applicants = reactive([
+  {
+    previous: '',
+    expiration: ''
+  }
+])
+
+function AddField() {
+  applicants.push({
+    previous: '',
+    expiration: ''
+  })
+
+  console.log('applicants', applicants)
+}
+
+</script>
+
+<template>
+  <div class='container-fluid py-2 '>
+    <div class='row gap-2'>
+      <div class='col'>
+        <label for='InvoiceNo' class='form-label'>Invoice No</label>
+        <input type='number' v-model='InvoiceDetails.InvoiceNumber' class='form-control' id='InvoiceNo'
+               placeholder='001'>
+      </div>
+      <div class='col'>
+        <label for='referenceNo' class='form-label'>Referance No.</label>
+        <input type='number' v-model='InvoiceDetails.referenceNumber' class='form-control' id='referenceNo'
+               placeholder='001'>
+      </div>
+      <div class='col'>
+        <label for='Date' class='form-label'>Date</label>
+        <input type='date' class='form-control' id='Date' placeholder='Thursday, 21-02-2019'>
+      </div>
+    </div>
+    <div class='row'>
+      <div class='col'>
+        <label for='PartyName' class='form-label'>Party A/c Name</label>
+        <input type='text' class='form-control' v-model='InvoiceDetails.AccountName' id='PartyName' placeholder='001'>
+      </div>
+      <div class='col'>
+<!--        <p class='mb-2 common-text'>Sales Ledger</p>-->
+                <label for='salesLadger' class='form-lab  el'>Sales Ledger</label>
+        <select class='form-select' id='salesLadger' aria-label='Default select example'
+                v-model='InvoiceDetails.SalesLedger'>
+          <option selected>Default</option>
+          <option value='1'>Amazon Sale</option>
+          <option value='2'>Flipcart</option>
+          <option value='3'>Alibaba</option>
+        </select>
+      </div>
+    </div>
+    <div class='row'>
+      <div class='col'>
+        <p class='m-0'>Address : Surat, Gujarat,</p>
+        <p class='m-0'>Phone No. : 95687-69846 , Email id : abc@gmail.com</p>
+      </div>
+      <div class='col'>
+        <p class='m-0'>Sale Account</p>
+      </div>
+    </div>
+    <!--    item list-->
+    <span class='my-3 text-black fs-4'>Item List</span>
+    <div class='row'>
+      <div class='col-4'><span>Name of item</span></div>
+      <div class='col-1'><span>Qty</span></div>
+      <div class='col-1'><span>UOM</span></div>
+      <div class='col-1'><span>Rate Per</span></div>
+      <div class='col-1'><span>GST Rate</span></div>
+      <div class='col-1'><span>Discount</span></div>
+      <div class='col-1'><span>Amount</span></div>
+    </div>
+    <div v-for='applicant in applicants'>
+      <form @submit.prevent='SaveProductDetails' v-on:input='SaveProductDetails'>
+        <div class='row py-2'>
+          <div class='col-4'>
+<!--            <label for='NameItem' class='form-label'>Name of item</label>-->
+            <input type='text' class='form-control' id='NameItem' v-model='ItemList.itemName' placeholder='lorem ipsums'>
+          </div>
+          <div class='col-1'>
+<!--            <label for='quality' class='form-label'>Qty</label>-->
+            <input type='number' class='form-control' id='quality' v-model='ItemList.Qlt' placeholder='10'>
+          </div>
+          <div class='col-1'>
+<!--            <label for='uom' class='form-label'>UOM</label>-->
+            <input type='number' class='form-control' id='uom' v-model='ItemList.Uom' placeholder='MRTS'>
+          </div>
+          <div class='col-1'>
+<!--            <label for='ratePer' class='form-label'>Rate Per</label>-->
+            <input type='number' class='form-control' id='ratePer' v-model='ItemList.Rate' placeholder='$30'>
+          </div>
+          <div class='col-1'>
+<!--            <label for='gstRate' class='form-label'>GST Rate</label>-->
+            <input type='number' class='form-control' v-model='ItemList.Gst' id='gstRate' placeholder='10'>
+          </div>
+          <div class='col-1'>
+<!--            <label for='discount' class='form-label'>Discount</label>-->
+            <input type='number' class='form-control' id='discount' v-model='ItemList.Discount' placeholder='10'>
+          </div>
+          <div class='col-2'>
+<!--            <label for='Amount' class='form-label'>Amount</label>-->
+            <input type='number' class='form-control' id='Amount' v-model='TotalAmount' placeholder='$10'>
+          </div>
+          <div class='col d-flex align-items-end'>
+            <i class='fa-solid fa-pen fs-5 px-1' style='cursor: pointer '></i>
+            <i class='fa-solid fa-trash fs-5 px-1' style='cursor: pointer '></i>
+            <i class='fa-solid fa-plus fs-5' style='cursor: pointer ' @click='AddField'></i>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class='row'>
+      <div class='col-4'>
+        <p class='fw-bold py-3'>Total</p>
+      </div>
+      <div class='col-5'>
+        <p class='fw-bold py-3'>{{ ItemList.Qlt === '' ? 0 : ItemList.Qlt }}</p>
+      </div>
+      <div class='col'>
+        <p class='fw-bold py-3'>{{ TotalAmount }}</p>
+      </div>
+    </div>
+
+    <div class='row'>
+      <div class='col-8'>
+        <p class='my-3 text-black fs-4'>Generate e-Way Bill</p>
+        <div class='form-check form-check-inline'>
+          <input class='form-check-input' type='checkbox' value='' id='flexCheckDefault'>
+          <label class='form-check-label' for='flexCheckDefault'>
+            No
+          </label>
+        </div>
+        <div class='form-check form-check-inline'>
+          <input class='form-check-input' type='checkbox' value='' id='GenarateEbook' checked>
+          <label class='form-check-label' for='GenarateEbook'>
+            Yes
+          </label>
+        </div>
+        <div class='w-50 mt-3'>
+          <label class='form-check-label' for='Narrotion'>
+            Narration
+          </label>
+          <textarea class='form-control' placeholder='001' id='Narrotion'></textarea>
+        </div>
+      </div>
+      <div class='col'>
+        <div class='form-check'>
+          <div class='d-flex align-items-center gap-2'>
+            <input class='form-check-input' type='checkbox' value='' id='flexCheckChecked' checked>
+            <span class='common-text'>Round Off</span>
+            <div class='row'>
+              <div class='col'>
+                <input type='number' class='form-control' id='roundOffId' v-model='TotalAmount'>
+              </div>
+            </div>
+          </div>
+          <div class='row pt-3'>
+            <div class='col-3 d-flex justify-content-start'>
+              <span class='common-text'>CGST</span>
+            </div>
+            <div class='col'>
+              <span class='common-text'>{{ GstPlus }}</span>
+            </div>
+          </div>
+          <div class='row'>
+            <div class='col-3 d-flex justify-content-start'>
+              <span class='common-text'>SGST</span>
+            </div>
+            <div class='col'>
+              <span class='common-text'>$1,23,567</span>
+            </div>
+          </div>
+          <div class='row'>
+            <div class='col-3 d-flex justify-content-start'>
+              <span class='common-text'>IGST</span>
+            </div>
+            <div class='col'>
+              <span class='common-text'>$1,23,567</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class='w-100 d-flex justify-content-end gap-4'>
+      <button type='button' class='btn btn-outline-secondary px-5 rounded-pill'>Cancel</button>
+      <button type='button' @click='SaveProductDetails' class='btn btn-primary px-5 rounded-pill'>Save</button>
+    </div>
+  </div>
+
+  <!--  <button @click='AddField'>Add Field</button>-->
+  <!--  <br>-->
+  <!--  <div class='previous' v-for='applicant in applicants'>-->
+  <!--    <span>{{ applicants.previous }}</span>-->
+  <!--    <label for='duration'>= Previous Visa:</label>-->
+  <!--    <input type='text' id='duration' v-model='applicant.previous' required>-->
+  <!--    <label for='duration1'>Year of expiration:</label>-->
+  <!--    <input type='text' id='duration1' v-model='applicant.expiration' required>-->
+  <!--  </div>-->
+</template>
+
+<style scoped>
+
+</style>
